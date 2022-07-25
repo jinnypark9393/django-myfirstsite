20c318f5ed9bbd1efab849e6e15304d0c90d86e9
Jinny Park <jinnypark9393@gmail.com>
2022-07-26 05:56:54 +0900

    220726 05:56 Jinny Installed database and testing APIs

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,21 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    # 데이터베이스의 필드값을 알기 쉽게 표시한다.
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    # 데이터베이스의 필드값을 알기 쉽게 표시한다.
+    def __str__(self):
+        return self.choice_text
